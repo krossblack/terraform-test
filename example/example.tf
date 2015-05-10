f190424091575e077154478def406fbfd1ec875b
krossblack777 <krossprogram@gmail.com>
2015-05-10 15:02:29 +0900

    Available ssh login

--- example/example.tf
+++ example/example.tf
@@ -1,4 +1,23 @@
 resource "aws_instance" "example" {
-    ami = "ami-4985b048"
+    ami = "ami-89634988"
     instance_type = "t2.micro"
+    key_name = "${var.key_name}"
+
+    # Our Security group to allow HTTP and SSH access
+    security_groups = ["${aws_security_group.default.name}"]
+}
+
+resource "aws_security_group" "default" {
+    name = "terraform_example"
+    description = "Used in the terraform"
+
+    # SSH access from anywhere
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
 }
